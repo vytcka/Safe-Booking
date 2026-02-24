07c01d106a37f4174f6f3c3b49dc112af13e4dd3
vytcka <c4002911@newcastle.ac.uk>
2026-02-24 11:39:45 -0800

    continueing work

--- src/manager.py
+++ src/manager.py
@@ -7,10 +7,10 @@ class Booking:
         self.time = datetime.now
         self.slot = slot
         
-    def hashfunc(self)-> int:
+    def __hash__(self)-> int:
         return hash(self.email, self.number, self.time, self.slot)
     
-    def compare(self, obj) -> bool:
+    def __eq__(self, obj) -> bool:
         if(self.email == obj.email and self.number == obj.email and self.slot == obj.slot and self.time == obj.time):
             return True
         return False
