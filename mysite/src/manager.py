b4e40f4d0df3980e6dda10bbae369b2ec157b3af
vytcka <c4002911@newcastle.ac.uk>
2026-02-25 12:29:29 -0800

    Changes to the Booking class

--- mysite/src/manager.py
+++ mysite/src/manager.py
@@ -1,33 +1,30 @@
-import datetime
+from datetime import date, timedelta
+from django.db import models
+from django.core.validators import MaxValueValidator
 
-class Booking:
-    def __init__(self, email, time, registered_time, number, slot):
-        self.email = email
-        self.number = number
-        self.time = time
-        self.slot = slot
-        self.registered_time = registered_time
-    
+class Booking(models.Model):
+    email = models.EmailField(max_length=100)
+    number = models.CharField(max_length=50)
+    time = models.DateField()
+    slot = models.PositiveIntegerField(validators=[MaxValueValidator(99999)])
+    registered_time = models.DateField()
+
     def expiringReservation(self):
-        if self.time + datetime.timedelta(days=365) < datetime.datetime.now():
-            # call delete reservation
-            #checkfunc.delete("from db WHERE name = $1 ")
-            #make db call
-            return
-        else:
-            return
-        
-    def __hash__(self)-> int:
-        return hash(self.email, self.number, self.time, self.slot, self.registered_time)
-    
-    def __eq__(self, obj) -> bool:
-        if(self.email == obj.email and self.number == obj.email and self.slot == obj.slot and self.time == obj.time and self.registered_time == obj.registered_time):
+        if self.time + timedelta(365) < date.today():
+            self.delete();
             return True
         return False
-    
 
-    
-        
+    def __hash__(self) -> int:
+        return hash((self.email, self.number, self.time, self.slot, self.registered_time))
 
-        
-
+    def __eq__(self, obj) -> bool:
+        if not isinstance(obj, Booking):
+            return False
+        return (
+            self.email == obj.email and
+            self.number == obj.number and
+            self.slot == obj.slot and
+            self.time == obj.time and
+            self.registered_time == obj.registered_time
+        )
