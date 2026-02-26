1781fdf8593e77ca6d3b9e5c79ca0850ab9de139
vytcka <c4002911@newcastle.ac.uk>
2026-02-26 04:08:35 -0800

    Booking class incorperated

--- mysite/src/manager.py
+++ mysite/src/manager.py
@@ -4,7 +4,8 @@ from django.core.validators import MaxValueValidator
 from django.core.exceptions import ValidationError
 
 class Booking(models.Model):
-    """The Booking class incoreperates Object-relational Mapping to create a  """
+    """The Booking class incoreperates Object-relational Mapping to create a map bookings on the backend and render
+    them in the frontend once changed."""
     email = models.EmailField(max_length=100)
     number = models.CharField(max_length=50)
     time = models.DateField()
@@ -12,13 +13,15 @@ class Booking(models.Model):
     registered_time = models.DateField()
     isValid = models.BooleanField(default=True)
 
-    def expiringReservation(self):
+    def expiringReservation(self)-> bool:
+        """Remove a expiring booking... can be changed but currently a year in advance."""
         if self.time + timedelta(365) < date.today():
             self.delete();
             return True
         return False
     
-    def oneReservationPerUser(self):
+    def oneReservationPerUser(self)-> bool:
+        """Making sure that a user can only make a single booking per user."""
         bookings = Booking.objects.filter(email=self.email)
         if bookings.count() < 2:
             self.isValid = True
@@ -31,9 +34,11 @@ class Booking(models.Model):
         
 
     def __hash__(self) -> int:
+        """Hashing object"""
         return hash((self.email, self.number, self.time, self.slot, self.registered_time))
 
     def __eq__(self, obj) -> bool:
+        """checking if the objects are the same."""
         if not isinstance(obj, Booking):
             return False
         return (
