20e5a64b4d33fd9e8184cbf29b190646e3e97c16
vytcka <c4002911@newcastle.ac.uk>
2026-02-27 10:51:50 -0800

    integrating mitigation

---
+++ mysite/bookings/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .manage import Booking
+
+admin.site.register(Booking)
+# Register your models here.
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path, include
+
 """
 URL configuration for mysite project.
 
